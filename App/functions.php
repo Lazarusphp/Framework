65f85205ed6929ae657c28d1ca1c9bdd4d3f2040
martin bamber <mbamber1986@gmail.com>
2024-09-30 00:58:11 +0100

    Added Now function to functions.php

--- App/functions.php
+++ App/functions.php
@@ -1,5 +1,5 @@
 <?php
-
+use LazarusPhp\DateManager\Date;
 function slug($slug)
 {
     return str_replace(" ","-",$slug);
@@ -10,3 +10,33 @@ function slug($slug)
      header("location:/$location");
 }
 
+ function now($time="hours|minute")
+ {
+     $timedformat = [];
+//     Current Format is utc
+    $date = "y-m-d";
+//    $time = "h:i:s";
+    $explode = explode("|",$time);
+    foreach ($explode as $t)
+    {
+        $timedformat[$t] = true;
+    }
+
+    $time = (object) $timedformat;
+    if(isset($time->hours))
+    {
+        ($time->hours == true) ? $output[] = "H" : $output[] =  "";
+    }
+
+     if(isset($time->minute))
+     {
+         ($time->minute == true) ? $output[] = "i" : $output[] = "" ;
+     }
+
+     if(isset($time->seconds))
+     {
+         ($time->seconds == true) ? $output[] = "s" : $output[] = "";
+     }
+     $time = implode(":",$output);
+     return Date::create("now")->format("$date $time");
+ }
