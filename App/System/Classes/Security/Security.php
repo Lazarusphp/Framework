13db84850e8a90ceac5e0561483e29baaf6b433a
mbamber1986 <mbamber1986@gmail.com>
2024-11-05 11:46:00 +0000

    Moved Encyption and decruption to security class

--- App/System/Classes/Security/Security.php
+++ App/System/Classes/Security/Security.php
@@ -8,6 +8,8 @@ use App\System\Classes\Validation\Validation;
 class Security
 {
     private $token;
+    private static $key = "";
+    private static $cipher = 'AES-256-CBC';
 
     public function __construct()
     {
@@ -42,5 +44,26 @@ class Security
         return (password_verify($local, $remote) == true) ? true : false;
     }
 
+
+    public static function encryptValue($value)
+    {
+        $iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length(self::$cipher));
+        $encrypted = openssl_encrypt($value, self::$cipher, self::$key, 0, $iv);
+    
+        // Combine IV and encrypted value for decryption later
+        return base64_encode($iv . $encrypted);
+    }
+    
+    public static function decryptValue($value)
+    {
+        $decoded = base64_decode($value);
+    
+        // Extract IV and encrypted data
+        $iv_length = openssl_cipher_iv_length(self::$cipher);
+        $iv = substr($decoded, 0, $iv_length);
+        $encrypted = substr($decoded, $iv_length);
+    
+        return openssl_decrypt($encrypted, self::$cipher, self::$key, 0, $iv);
+    }
     
 }
