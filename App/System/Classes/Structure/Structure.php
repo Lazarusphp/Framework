eb325a0c9a84228057e4c24ad733a4dfeb4bb7ea
mbamber1986 <mbamber1986@gmail.com>
2025-01-05 00:35:19 +0000

    Added new Create and fetch Values to Support new Ini Functionality

--- App/System/Classes/Structure/Structure.php
+++ App/System/Classes/Structure/Structure.php
@@ -1,13 +1,84 @@
 <?php
 namespace App\System\Classes\Structure;
 use App\System\Classes\ErrorHandler;
+use Closure;
+use FireCore\IniWriter\Handler;
+
 use function PHPSTORM_META\elementType;
 class Structure extends StructureConfig
 {
+
+    private $dir = [];
+    private $pathname = [];
+
+
+    private static $path = [];
+    private static $data = [];
+
+
+
+
+
+    private $iniFile;
     public function __construct()
     {
         $this->generateRoot();
         $this->generatePaths();
+    }
+
+
+    public static function create($name,$file,array $data):void
+    {
+        self::$path[$name] = $file;
+        // Set new Handler Instance
+        self::$data[$name] = $data;
+        // Open the Ini file,
+        Handler::open($file);
+
+        foreach($data as $key => $value)
+        {
+            Handler::set($name,$key,$value);
+        }
+
+        Handler::save();
+
+        Handler::close();
+        
+    }
+    public static function fetchValue($name,$key):mixed
+    {
+        Handler::open(self::$path[$name]);
+        return Handler::get($name,$key);
+    }
+
+
+
+    public static function getPath($name=null)
+    {
+        if($name === null)
+        {
+            return self::$path;
+        }
+        else
+        {
+            return self::$path[$name]; 
+        }
+       
+    }
+
+
+
+
+
+    //TODO:  Generate an array to t ie in with the INI File Class
+    private function defaultIni()
+    {
+        $this->dir = [
+            "Storage" => ROOT. DIRECTORY_SEPARATOR ."Storage",
+        ];
+
+
+        var_dump($this->dir);
     }
 
     // TODO: Remove this method No longer needed
@@ -33,6 +104,28 @@ class Structure extends StructureConfig
         $this->newConstant($name, $path);
     }
     // Load Paths
+
+
+    //Todo Add Private Generate Root path Here
+
+    protected function generateRoot()
+    {
+
+        $allowedDir = ["public_html","public","www"];
+
+        foreach ($allowedDir as $dir)
+        {
+            if(is_dir("../$dir"))
+                {
+                    $explode = explode(DIRECTORY_SEPARATOR, getcwd());
+                    array_pop($explode);
+                    $this->root = implode(DIRECTORY_SEPARATOR, $explode);
+                }
+        }
+
+        define("ROOT", $this->root);
+        return $this->root;
+    }
 
 
     public function hasFile($name)  :bool  
