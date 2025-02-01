d4a5b30b56ba52113cfd93cdb71541f4528ee55e
martin bamber <mbamber1986@gmail.com>
2025-02-01 23:52:11 +0000

    Added Ability to Pass Config Files to SessionWriter

--- App/System/Writers/SessionWriter.php
+++ App/System/Writers/SessionWriter.php
@@ -8,30 +8,24 @@ use PDO;
 class SessionWriter Implements SessionHandlerInterface
 {
 
-    private $config = [
-        "table"=>"sessions",
-        "expiry"=>1,
-        "format"=>"Y-m-d H:i:s"
-    ];
+    private $config;
     private $date;
 
-    public function test()
+    public function __construct(array $config)
     {
-        return "Hello World";
+        $this->config = $config;
+        $required = ["days","table"];
+        if(is_array($config))
+        {
+            foreach ($required as $key) {
+                if (!array_key_exists($key,$config)) {
+                    throw new \InvalidArgumentException("Missing required config key: $key");
+                }
+            }
+        } 
     }
 
-    public function customBoot():void
-    {
-        $this->setCookie();
-    }
-
-    public function setCookie() :bool
-    {
-        return  setcookie(session_name(), session_id(), Date::asTimestamp(Date::withAddedTime("now","P".$this->config["expiry"]."D")), "/", "." . $_SERVER['HTTP_HOST']);
-            
-    }
-
-    public function open($path,$name):bool
+    public function open(?string $path,?string $name):bool
     {
         return true;
     }
@@ -43,24 +37,24 @@ class SessionWriter Implements SessionHandlerInterface
     public function read(string $sessionID):string | false
     {
         $query = new QueryBuilder();
-        $stmt = $query->sql("SELECT * FROM sessions WHERE session_id = :sessionID",[":sessionID"=>$sessionID])
+        $stmt = $query->sql("SELECT * FROM ". $this->config["table"] ." WHERE session_id = :sessionID",[":sessionID"=>$sessionID])
         ->one(PDO::FETCH_ASSOC);
         return $stmt ? $stmt['data'] : '';
     }
 
     public function write($sessionID,$data):bool
     {
-        $date = Date::create("now")->format("Y-m-d H:i:s");
+        $date = Date::withAddedTime("now","P".$this->config["days"]."D")->format("y-m-d h:i:s");  
         $params =  [":sessionID"=>$sessionID,":data"=>$data,":expiry"=>$date];
         $query = new QueryBuilder();
-        $query->asQuery("REPLACE INTO sessions (session_id,data,expiry) VALUES(:sessionID,:data,:expiry)",$params);
+        $query->asQuery("REPLACE INTO ". $this->config["table"] . "  (session_id,data,expiry) VALUES(:sessionID,:data,:expiry)",$params);
         return true;
     } 
     public function destroy($sessionID): bool
     {
         $query = new QueryBuilder();
         $params = [":sessionID"=>$sessionID];
-        $query->asQuery("DELETE FROM sessions WHERE session_id=:sessionID",$params);
+        $query->asQuery("DELETE FROM ". $this->config["table"] . "  WHERE session_id=:sessionID",$params);
         return true;
     }
 
@@ -72,7 +66,7 @@ class SessionWriter Implements SessionHandlerInterface
         try {
             $query = new QueryBuilder();
             $params = [":expiry"=>$expiry];
-            $query->asQuery("DELETE FROM sessions WHERE expiry  < :expiry",$params);
+            $query->asQuery("DELETE FROM ". $this->config["table"] . "  WHERE expiry  < :expiry",$params);
         } catch (PDOException $e) {
             throw new PDOException($e->getMessage() . $e->getCode());
         }
