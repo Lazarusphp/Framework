9cd53944060b652c35d1dc637c2d4def6f53057a
martin Bamber <mbamber1986@gmail.com>
2025-02-01 22:58:56 +0000

    updated App to support new changes

--- App/System/App.php
+++ App/System/App.php
@@ -22,6 +22,7 @@ class App  extends Structure
         ini_set("display_startup_errors",1);
         error_reporting(E_ALL);
         self::generateRoot();
+        
         // AutoLoad the Path before Calling the file.
         // self::mapPath(ROOT."/Storage/Paths.ini","Paths");
         $this->boot();
@@ -30,24 +31,16 @@ class App  extends Structure
     public function loadRouter():void
     {  
         $folder = ROOT."/Storage";
-        if(self::hasDirectory($folder) === true)
-        {
-            if(!chmod($folder,0777))
-            {
-                trigger_error("Storage Directory is not writable ");
-            }
-            else
-            { 
-                // echo self::fetch("Paths","Router");
-                include_once(ROOT."/App/System/Router/router.php");
-            }
-       }
+
+        if (!is_writable($folder)) {
+            throw new \RuntimeException('Storage directory is not writable: ' . $folder);
+        }
         else
         {
-            trigger_error("Storage Directory is missing Please Create it");
+            include_once(ROOT."/App/System/Router/router.php");
         }
-        
-    }
+                // echo self::fetch("Paths","Router");
+   }
 
     public function boot()  :void
     {
