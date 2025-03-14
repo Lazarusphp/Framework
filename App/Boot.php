8dc975b455c3ebbd73ca557b1bcbccba868bb391
martin bamber <mbamber1986@gmail.com>
2025-03-14 23:08:47 +0000

    Updated boot file, removed the destroy method

--- App/Boot.php
+++ App/Boot.php
@@ -6,18 +6,19 @@ use App\System\Classes\Security\Security;
 use App\System\Classes\Requests\Requests;
 use App\System\Classes\Validation\Validation;
 use App\System\Classes\VersionControl\VersionControl;
-use FireCore\DataHandler\Writer;
+use LazarusPhp\FileCrafter\Writer;
 
 class Boot
 {
 
     public static function run()
     {
-        $app = new App(false);
+        $app = new App(true);
         if($app->versionControl())
         {
            self::loadVc();
         }
+        
         $app->loadRouter();
         // Code to run the application
     }
@@ -34,14 +35,13 @@ class Boot
                 $instance = new $class();
                 if (property_exists($instance, 'vname') && property_exists($instance, 'vno') && property_exists($instance, 'lup')) {
                  
-                   
                     Writer::generate("Versions", function($writer) use ($instance)
                     {
-                        $vname = $instance->vname;
+                        $vname = str_replace(" ","_",$instance->vname);
                         $vno = $instance->vno;
                         $lup = $instance->lup;
-                        $writer->section($vname)->set("Script Version", $vno);
-                        $writer->section($vname)->set("Last Updated", $lup);
+                        $writer->section($vname)->set("version_number", $vno);
+                        $writer->section($vname)->set("last_updated", $lup);
                         $writer->save();
                     });
                 } else {
