837667e55e26c97d1611c190886e4b15ebd7d1b5
martin bamber <mbamber1986@gmail.com>
2025-04-06 18:05:32 +0100

    Created a Class Injector

---
+++ App/System/Classes/ClassInjector/Injector.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\System\Classes\ClassInjector;
+
+class Injector
+{
+    private static $classname;
+    private static $method;
+    private static $args;
+    private $required = false;
+    private $missingClasses = [];
+    // Class implementation goes here#
+
+    public static function inject(string $class, string $method, ...$args)
+    {
+        self::$classname = $class;
+        self::$method = $method;
+        self::$args = $args;
+        return new self();
+    }
+
+    public function required(...$classes)
+    {
+        foreach ($classes as $class) {
+            if (!class_exists($class)) {
+                $this->missingClasses[] = "Cannot locate Class $class"; 
+            }
+        }
+
+        if(count($this->missingClasses) >= 1)
+        {
+            $this->required = true;
+        }
+        else
+        {
+            $this->required = false;
+        }
+        return $this;
+    }
+
+    public function save()
+    {
+        if (!$this->required) {
+            if (class_exists(self::$classname)) {
+                if (method_exists(self::$classname, self::$method)) {
+                    return call_user_func_array([self::$classname, self::$method], self::$args);
+                }
+            }
+        }
+        else
+        {
+            foreach($this->missingClasses as $message)
+            {
+                trigger_error($message);
+            }
+        }
+    }
+}
