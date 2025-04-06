fc19ca5c8936d9bac7ba361d34800c26ee912853
martin bamber <mbamber1986@gmail.com>
2025-04-06 23:20:52 +0100

    Refactored code

--- App/Boot.php
+++ App/Boot.php
@@ -4,14 +4,6 @@ namespace App;
 
 use App\System\App;
 use App\System\BootManager\Loader;
-use App\System\Classes\Requests\Requests;
-use App\System\Classes\VersionControl\VersionControl;
-use App\System\Writers\SessionWriter;
-use LazarusPhp\LazarusDb\QueryBuilder;
-use LazarusPhp\SessionManager\Sessions;
-use LazarusPhp\SessionManager\SessionsFactory;
-use App\System\Classes\ClassInjector\Injector;
-use LazarusPhp\LazarusDb\Connection;
 
 class Boot extends Loader
 {
@@ -23,9 +15,8 @@ class Boot extends Loader
         iniControl();
         self::generateRoot();
         self::setEnv();
-        self::loadConnection("");
+        self::loadConnection();
         self::loadRouter();
-        self::run();
     }
 
 }
