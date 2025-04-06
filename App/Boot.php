1786b94dceadd5e3b8886d2ec1265b349e60cf53
martin bamber <mbamber1986@gmail.com>
2025-04-06 16:11:52 +0100

    removed LoadVc and commented above LoadDatabase

--- App/Boot.php
+++ App/Boot.php
@@ -18,12 +18,8 @@ class Boot
     public static function run()
     {
         $app = new App();
-        if($app->versionControl())
-        {
-           self::loadVc();
-        }
-
-        $app->loadDatabase(ROOT."/Configs/Config.php");
+        // Leave blank to load from env file
+        $app->loadDatabase();
 
         SessionsFactory::instantiate([SessionWriter::class],["table" => "sessions","days" => 30]);
         SessionsFactory::set("username","mike");;
@@ -31,33 +27,33 @@ class Boot
         // Code to run the application
     }
 
-    public static function loadVc()
-    {
-        $classes = [
-            'Requests' => Requests::class,
-            'VersionControl' => VersionControl::class,
-        ];
+    // public static function loadVc()
+    // {
+    //     $classes = [
+    //         'Requests' => Requests::class,
+    //         'VersionControl' => VersionControl::class,
+    //     ];
 
-        foreach ($classes as $name => $class) {
-            if (class_exists($class)) {
-                $instance = new $class();
-                if (property_exists($instance, 'vname') && property_exists($instance, 'vno') && property_exists($instance, 'lup')) {
+    //     foreach ($classes as $name => $class) {
+    //         if (class_exists($class)) {
+    //             $instance = new $class();
+    //             if (property_exists($instance, 'vname') && property_exists($instance, 'vno') && property_exists($instance, 'lup')) {
                  
-                    Writer::generate("Versions", function($writer) use ($instance)
-                    {
-                        $vname = str_replace(" ","_",$instance->vname);
-                        $vno = $instance->vno;
-                        $lup = $instance->lup;
-                        $writer->section($vname)->set("version_number", $vno);
-                        $writer->section($vname)->set("last_updated", $lup);
-                        $writer->save();
-                    });
-                } else {
-                    echo "$name class does not have all required properties.\n";
-                }
-            } else {
-                echo "$name class does not exist.\n";
-            }
-        }
-    }
+    //                 Writer::generate("Versions", function($writer) use ($instance)
+    //                 {
+    //                     $vname = str_replace(" ","_",$instance->vname);
+    //                     $vno = $instance->vno;
+    //                     $lup = $instance->lup;
+    //                     $writer->section($vname)->set("version_number", $vno);
+    //                     $writer->section($vname)->set("last_updated", $lup);
+    //                     $writer->save();
+    //                 });
+    //             } else {
+    //                 echo "$name class does not have all required properties.\n";
+    //             }
+    //         } else {
+    //             echo "$name class does not exist.\n";
+    //         }
+    //     }
+    // }
 }
