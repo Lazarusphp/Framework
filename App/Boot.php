c7481c4299c716d80403bd8a69b2cd512cde78b2
martin bamber <mbamber1986@gmail.com>
2025-04-06 16:58:04 +0100

    Updated Boot.php to support new BootManager changes

--- App/Boot.php
+++ App/Boot.php
@@ -3,57 +3,23 @@
 namespace App;
 
 use App\System\App;
+use App\System\BootManager\Loader;
 use App\System\Classes\Requests\Requests;
 use App\System\Classes\VersionControl\VersionControl;
 use App\System\Writers\SessionWriter;
-use LazarusPhp\LazarusDb\Connection;
 use LazarusPhp\LazarusDb\QueryBuilder;
-
 use LazarusPhp\SessionManager\Sessions;
 use LazarusPhp\SessionManager\SessionsFactory;
 
-class Boot
+class Boot extends Loader
 {
 
     public static function run()
     {
-        $app = new App();
-        // Leave blank to load from env file
-        $app->loadDatabase();
-
-        SessionsFactory::instantiate([SessionWriter::class],["table" => "sessions","days" => 30]);
-        SessionsFactory::set("username","mike");;
-        $app->loadRouter();
+        // Load class arrays must be in order
+        require_once("../App/functions.php");
+        iniControl();
+       self::classLoader("generateRoot","setEnv","loadConnection","loadRouter");
         // Code to run the application
     }
-
-    // public static function loadVc()
-    // {
-    //     $classes = [
-    //         'Requests' => Requests::class,
-    //         'VersionControl' => VersionControl::class,
-    //     ];
-
-    //     foreach ($classes as $name => $class) {
-    //         if (class_exists($class)) {
-    //             $instance = new $class();
-    //             if (property_exists($instance, 'vname') && property_exists($instance, 'vno') && property_exists($instance, 'lup')) {
-                 
-    //                 Writer::generate("Versions", function($writer) use ($instance)
-    //                 {
-    //                     $vname = str_replace(" ","_",$instance->vname);
-    //                     $vno = $instance->vno;
-    //                     $lup = $instance->lup;
-    //                     $writer->section($vname)->set("version_number", $vno);
-    //                     $writer->section($vname)->set("last_updated", $lup);
-    //                     $writer->save();
-    //                 });
-    //             } else {
-    //                 echo "$name class does not have all required properties.\n";
-    //             }
-    //         } else {
-    //             echo "$name class does not exist.\n";
-    //         }
-    //     }
-    // }
 }
