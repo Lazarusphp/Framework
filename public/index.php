aaaeabce2679f77d5f486403c4c802778753620a
martin bamber <mbamber1986@gmail.com>
2025-05-23 17:19:34 +0100

    Updated index file

--- public/index.php
+++ public/index.php
@@ -1,9 +1,13 @@
 <?php
 
 use App\Boot;
-
+use LazarusPhp\AuthControl\Auth;
+use LazarusPhp\AuthControl\CoreFiles\AuthCore;
+use LazarusPhp\LazarusDb\QueryBuilder;
+use LazarusPhp\SecurityFramework\Hash;
 
 require(__DIR__ . "/../vendor/autoload.php");
+
 if (class_exists('App\Boot'))
 {
     new Boot();
