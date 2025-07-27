62873cd486749db767f93536d4c9c7fbc7e785ad
martin bamber <mbamber1986@gmail.com>
2025-07-27 11:15:35 +0100

    Updated Boot to support new Changes within Core Folder

--- App/Boot.php
+++ App/Boot.php
@@ -4,7 +4,8 @@ namespace App;
 
 use App\Http\Model\Users;
 use App\System\App;
-use App\System\BootManager\Loader;
+use App\System\Core\BootLoader;
+use App\System\Core\Functions;
 use LazarusPhp\LazarusDb\Database\Connection;
 use LazarusPhp\LazarusDb\QueryBuilder\QueryBuilder;
 use LazarusPhp\LazarusDb\SchemaBuilder\Schema;
@@ -13,14 +14,14 @@ use LazarusPhp\SessionManager\Writers\SessionWriter;
 use LazarusPhp\LazarusDb\SchemaBuilder\SchemaLoader;
 use LazarusPhp\LazarusDb\SchemaBuilder\Table;
 
-class Boot extends Loader
+class Boot extends BootLoader
 {
 
     public function __construct()
     {
-        require_once("../App/functions.php");
+        $functions = new Functions();
 
-        iniControl();
+        $functions->iniControl();
         self::generateRoot();
         self::setEnv();
         // Get Connection Status
@@ -30,8 +31,10 @@ class Boot extends Loader
          * echoing anything before will get cause errors and warnings.
          */
 
-        $session = new Sessions();
-        $session->instantiate([SessionWriter::class],["days"=>365,"httponly"=>false,"secure"=>false]);
+        // $session = new Sessions();
+        // $session->instantiate([SessionWriter::class],["days"=>365,"httponly"=>false,"secure"=>false]);
     
+
+        SchemaLoader::load(__DIR__."/../Migrations/Schemas");
     }
 }
