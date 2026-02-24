cd3746aef789d0046a8cf946fcf78782fb852836
mbamber1986 <30671636+mbamber1986@users.noreply.github.com>
2026-02-24 15:13:12 +0000

    updated index to support new changes with providers and Boot file restructure

--- public/index.php
+++ public/index.php
@@ -1,22 +1,23 @@
 <?php
 ob_start();
+
 use LazarusPhp\Exceptions\Exceptions\FileNotFoundException;
 use LazarusPhp\Foundation\PathResolver\Resolve;
 
-require(__DIR__ . "/../vendor/autoload.php");
+require_once(__DIR__ . "/../App/App.php");
+require_once(__DIR__ . "/../vendor/autoload.php");
 // Autoload Dispatcher as Global
-Resolve::init(__DIR__,1);
+Resolve::init(__DIR__, 1);
 
-if (!class_exists('App\Boot'))
-{
+
+
+if (!class_exists('App\Boot')) {
     throw new FileNotFoundException("Boot Class does not Exist");
 }
 
-try{
-include_once(Resolve::get("Root")."/Bootloader/Providers.php");
-}
-catch(FileNotFoundException $e)
-{
+try {
+    loadFile(Resolve::get("Root") . "/Bootloader/Providers.php");
+} catch (FileNotFoundException $e) {
     echo "Bootloader/Providers.php file not found";
     exit;
 }
