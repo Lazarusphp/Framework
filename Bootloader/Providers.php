7b15993d4e178bd4ec8c68bc7846fcca27f707e0
martin bamber <mbamber1986@gmail.com>
2026-03-07 21:47:23 +0000

    Updated bootLoader Provider List

--- Bootloader/Providers.php
+++ Bootloader/Providers.php
@@ -5,10 +5,12 @@ use App\Providers\DatabaseProvider;
 use App\Providers\EnvProvider;
 use App\Providers\MapProvider;
 use App\Providers\SessionsProvider;
+use App\Providers\HttpKernelProvider;
 use LazarusPhp\Foundation\Providers\Providers;
 
 $container = Providers::map([EnvProvider::class,
 DatabaseProvider::class,
 SessionsProvider::class,
+HttpKernelProvider::class,
 MapProvider::class])->create();
 new Boot($container);
