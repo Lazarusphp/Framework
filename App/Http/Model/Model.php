1d687c67ad602826557fc47b1bc83e5d64004fbd
mbamber1986 <30671636+mbamber1986@users.noreply.github.com>
2026-03-09 00:26:34 +0000

    Updated Models

--- App/Http/Model/Model.php
+++ App/Http/Model/Model.php
@@ -1,22 +1,58 @@
 <?php
 namespace App\Http\Model;
-use LazarusPhp\QueryBuilder\Builder;
+
+use Exception;
+use LazarusPhp\QueryBuilder\QueryBuilder;
 use ReflectionClass;
 
-class Model extends Builder
+class Model
 {
-    protected $allowed = [];
-    protected $filtered = [];
+    protected array $attributes = [];
+    protected array $fillable = [];
+    protected array $guarded = [];
 
-    public static function __callStatic($method, $params)
+    // Magic setter stores values for the builder
+    public function __set($key, $value)
     {
-         $class = static::class;
-         $class = new ReflectionClass($class)->getShortName();
-         echo strtolower(str_replace(" ","_",$class));
+        $this->attributes[$key] = $value;
     }
 
-    public function hello()
+    // Magic getter
+    public function __get($key)
     {
-        echo "Hello";
+        return $this->attributes[$key] ?? null;
     }
+
+    public function builder($table):QueryBuilder
+    {
+        $builder = new QueryBuilder($table);
+                if (!empty($this->fillable)) {
+            $builder->fillable($this->fillable);
+        }
+        if (!empty($this->guarded)) {
+            $builder->guarded($this->guarded);
+        }
+        return $builder;
+    }
+
+ 
+     public static function __callStatic($method, $params)
+    {
+        $instance = new static();
+        return $instance->__call($method, $params);
+    }
+
+    public function __call($method, $params)
+    {
+        $table = strtolower((new ReflectionClass($this))->getShortName());
+        $builder = $this->builder($table);
+        if(!empty($this->attributes)){
+         // If CRUD operation, pass stored attributes automatically
+            if (in_array($method, ['insert', 'update'])) {
+                array_unshift($params, $this->attributes);
+            }
+        }
+        return $builder->$method(...$params);
+    }
+
 }
--- App/Http/Model/Users.php
+++ App/Http/Model/Users.php
@@ -5,5 +5,9 @@ class Users extends Model
 {
     // Store all “real” methods in an internal array
 
+    protected $allowed = [];
+    protected $filtered = [];
+
+    
 
 }
